e36920e75e43bd77e4f234db2e116f2a73596d56
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-03-18 05:26:33 +0000

    Update CustomMFA.psm1

--- CustomMFA.psm1
+++ CustomMFA.psm1
@@ -1,17 +1,23 @@
 function Get-OTP {
-    # Add your OTP generation logic here. For example, you can use TOTP or HOTP algorithms.
-    # In this example, we generate a simple 6-digit OTP.
-    return (Get-Random -Minimum 100000 -Maximum 999999)
+    param(
+        [string]$Secret
+    )
+
+    # Use the TOTP algorithm from the OTPCredentialProvider module
+    $otp = New-Totp -Secret $Secret -AsPlainText
+    return $otp
 }
 
-function Send-OTP([int]$OTP) {
+
+function Send-OTP([int]$OTP, [string]$Destination) {
     # Add your logic for sending the OTP to the user. You can use email, SMS, or a push notification, depending on the MFA provider.
-    # In this example, we just print the OTP to the console for demonstration purposes.
-    Write-Host "Sending OTP: $OTP"
+    # In this example, we still print the OTP to the console for demonstration purposes.
+    # You can replace this with your actual OTP sending logic.
+    Write-Host "Sending OTP: $OTP to destination: $Destination"
 }
 
-function Verify-OTP([int]$OTP, [int]$UserInput) {
-    # Add your logic for verifying the OTP provided by the user. You can call your MFA provider's API to verify the OTP.
-    # In this example, we compare the generated OTP with the user input.
-    return ($OTP -eq $UserInput)
+function Verify-OTP([int]$OTP, [int]$UserInput, [string]$Secret) {
+    # Verify the OTP provided by the user using the TOTP algorithm from the OTPCredentialProvider module
+    $generatedOTP = Get-OTP -Secret $Secret
+    return ($generatedOTP -eq $UserInput)
 }
